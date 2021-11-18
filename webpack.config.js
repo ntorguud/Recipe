496eb9345f2f011055dad0655732213d92b55e72
Naran Torguud <ntorguud@gmail.com>
2021-11-18 17:29:25 +0800

    Babel configuration

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,22 @@ module.exports = {
       template: 'src/index.html'
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ]
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
