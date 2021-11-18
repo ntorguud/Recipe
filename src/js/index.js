88301102df28b72d526c6a5a80dd7480acc2ac3a
Naran Torguud <ntorguud@gmail.com>
2021-11-18 21:10:10 +0800

    Create Model and View folders, and first JS codes

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+/// MVC -Model, View, Controller Archictecture
+import query from "./model/search";
+import {add} from "./view/searchView";
+
+console.log(`Hailt ${query}`);
+console.log(`Amount of two numbers is ${add(4, 6)}`);
