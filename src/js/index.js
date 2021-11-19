b79cd015a43ad521f015c9735acf99bcea90a9d0
Naran Torguud <ntorguud@gmail.com>
2021-11-19 14:44:10 +0800

    Add state and search controller

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,38 @@
 require("@babel/polyfill");
 import Search from "./model/search";
 
-let search = new Search("pasta");
+/**
+ * Web app state
+ * - Search query, result
+ * - Found recipe
+ * - Liked recipes
+ * - Ingredients of selected recipe
+ */
 
-search.doSearch()
-.then(r => console.log(r));
+const state = {};
+
+const controlSearch = async() => {
+    //1. Web-ees hailtiin key wordiiig gargarj avna.
+    const query = "pizza";
+
+
+    if(query) {
+        //2. New hailtiin object uusgej, state ruu search object hiine.
+        state.search = new Search(query);
+
+        //3. Hailt hiihed zoriulj UI-iig beldene.
+
+
+        //4. Execute search
+        await state.search.doSearch();
+    
+        //5. Show result into the display.
+        console.log(state.search.result);
+    }
+};
+
+//Let's connect event listener
+document.querySelector(".search").addEventListener("submit", e => {
+    e.preventDefault();
+    controlSearch();
+});
