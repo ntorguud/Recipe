b4cc5a76526453b06a27de1948fa44db6da72d89
Naran Torguud <ntorguud@gmail.com>
2021-11-19 13:48:42 +0800

    Get data from library with Axios

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,20 @@
 /// MVC -Model, View, Controller Archictecture
-import query from "./model/search";
-import {add} from "./view/searchView";
+/// No fetch(); today
+require("@babel/polyfill");
+import axios from "axios";
 
-console.log(`Hailt ${query}`);
-console.log(`Amount of two numbers is ${add(4, 6)}`);
+async function doSearch(search) {
+    try{
+        let result = await axios(`https://forkify-api.herokuapp.com/api/search?q= ${search}`);
+
+        const recipes = result.data.recipes; 
+
+        console.log(recipes);
+
+        result = await axios(`https://forkify-api.herokuapp.com/api/get?rId= ${recipes[1].recipe_id}`);
+        console.log(result);
+    } catch {
+        alert(`Error! ${error}`);
+    }  
+}
+doSearch("pizza");
